be462c3d4a88fc4468c9fb3cde17fb3218c917b1
Benjamin Zachariae <benjamin.zachariae@gmail.com>
2025-03-09 20:30:48 +0100

    deploy resume

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,15 @@ const nextConfig = {
 	experimental: {
 		reactCompiler: true,
 	},
+
 	eslint: {
 		// Warning: This allows production builds to successfully complete even if
 		// your project has ESLint errors.
 		ignoreDuringBuilds: true,
 	},
+	typescript: {
+		ignoreBuildErrors: true, // TODO: remove
+	}
 };
 
 export default nextConfig;
